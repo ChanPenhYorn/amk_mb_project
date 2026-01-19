172bffb72aec100368fd6bbd209b3167e1451d31
Chanpenh.Yorn <chanpenh.yorn@BNK-DKT0016.local>
2026-01-19 16:44:33 +0700

    update firebase flavor

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,38 @@
-import 'package:amk_bank_project/presentation/controllers/theme/theme_notifier.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:amk_bank_project/presentation/views/auth/login_screen.dart';
+import 'package:amk_bank_project/core/utils/app_logger.dart';
 
-void main() {
-  runApp(const ProviderScope(child: MyApp()));
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
+import 'flavors.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  F.appFlavor = Flavor.values.firstWhere(
+    (element) => element.name == appFlavor,
+  );
+
+  WidgetsFlutterBinding.ensureInitialized();
+  FirebaseApp firebaseApp = await Firebase.initializeApp();
+  await logAppInfo(firebaseApp);
+  runApp(const ProviderScope(child: App()));
 }
 
-class MyApp extends ConsumerWidget {
-  const MyApp({super.key});
+Future<void> logAppInfo(FirebaseApp firebaseApp) async {
+  final info = await PackageInfo.fromPlatform();
+  AppLogger.logInfo("🔥 App info:");
+  AppLogger.logInfo("  - App Name: ${info.appName}");
+  AppLogger.logInfo("  - Package Name / Bundle ID: ${info.packageName}");
+  // AppLogger.logInfo("  - Version: ${info.version}");
+  // AppLogger.logInfo("  - Build Number: ${info.buildNumber}");
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final themeState = ref.watch(themeProvider);
-    return MaterialApp(
-      title: 'AMK Bank',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light(),
-      darkTheme: ThemeData.dark(),
-      themeMode: themeState.isDarkMode == true
-          ? ThemeMode.dark
-          : ThemeMode.light,
-      home: LoginScreen(),
-    );
-  }
+  // AppLogger.logInfo("🔥 Firebase info:");
+  // AppLogger.logInfo("  - App ID: ${firebaseApp.options.appId}");
+  // AppLogger.logInfo("  - Project ID: ${firebaseApp.options.projectId}");
+  // AppLogger.logInfo("  - API Key: ${firebaseApp.options.apiKey}");
+  // AppLogger.logInfo(
+  //   "  - Messaging Sender ID: ${firebaseApp.options.messagingSenderId}",
+  // );
 }
